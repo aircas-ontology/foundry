1177eabd5404b2783797440e124617df94891c35
cuiwl <cuiwl@aricas.com>
2024-01-27 17:30:01 +0800

    workable except backing data source id

--- ontology/ontology-server/src/main/java/com/aircas/ptr/foundry/ontology/entity/bo/OntologyMetaBO.java
+++ ontology/ontology-server/src/main/java/com/aircas/ptr/foundry/ontology/entity/bo/OntologyMetaBO.java
@@ -23,6 +23,7 @@ public class OntologyMetaBO {
      * 图标
      */
     @ApiModelProperty(name = "icon", value = "本体图标", dataType = "java.lang.String", example = "飞机图标base64")
+    @
     private String icon;
 
     /**
