a01583e74f27e3438af00942a246c30f24164b2a
cuiwl <cuiwl@aricas.com>
2024-03-20 11:07:50 +0800

    save

--- groovyTest.groovy
+++ groovyTest.groovy
@@ -1,10 +1,24 @@
 import com.aircas.ptr.foundry.ontology.entity.objectType.*;
+import com.aircas.ptr.foundry.Javassit.*
+import javassist.CtClass;
+//import Javassist.JC;
 
 class Test {
 
     String handle(HashMap parameters) {
-        XTMB xtmb = new XTMB();
-        xtmb.mbbh = "3332"
-        return xtmb.mbbh;
+        ClassPool pool = ClassPool.getDefault();
+        CtClass ctClass = pool.get("Javassist.JC");
+        JC jc = (JC)CtClass.newInstance();
+        return 3;
+//        System.out.println(ctClass.toString());
+//        JC jc = new JC("1");
+//        System.out.println(jc.toString());
+//        XTMB xtmb = new XTMB();
+//        xtmb.mbbh = "3332"
+//        return xtmb.mbbh;
+    }
+
+    String handle(JC xtmb) {
+
     }
 }
--- ontology/ontology-server/src/main/java/com/aircas/ptr/foundry/ontology/OntologyClassGenerator.java
+++ ontology/ontology-server/src/main/java/com/aircas/ptr/foundry/ontology/OntologyClassGenerator.java
@@ -2,13 +2,16 @@ package com.aircas.ptr.foundry.ontology;
 
 import com.aircas.ptr.foundry.ontology.entity.bo.OntologyMetaBO;
 import com.aircas.ptr.foundry.ontology.entity.vo.OntologyPropertyVO;
-import javassist.ClassPool;
-import javassist.CtClass;
+import javassist.*;
+import javassist.bytecode.AccessFlag;
 import org.apache.poi.hssf.record.DVALRecord;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.List;
-
-
+@Service
 public class OntologyClassGenerator {
 
 
@@ -20,8 +23,25 @@ public class OntologyClassGenerator {
         //
              //   List<OntologyPropertyVO> selectByOntologyUniqueIdentifier(String uniqueIdentifier);
         ClassPool pool = ClassPool.getDefault();
-        CtClass ct = pool.makeAnnotation("top.ss007.GenerateClass");
-        ct.setInterfaces(new CtClass[]{pool.makeInterface("java.lang.Cloneable")});
+        CtClass ct = pool.makeClass("Javassist.JC");
+        try {
+            //添加属性
+            CtClass stringClass = pool.get("java.lang.String");
+            CtField f = new CtField(stringClass,"id",ct);
+            f.setModifiers(AccessFlag.PUBLIC);
+            ct.addField(f);
+            //添加构造函数
+            ct.addConstructor(CtNewConstructor.make("public JC(String pid){this.id=pid;}",ct));
+
+            ct.writeFile();
+            Field[] fields = ct.toClass().getFields();
+            System.out.println(fields[0].getName()+fields[0].getType());
+        } catch (NotFoundException | CannotCompileException | IOException e) {
+            e.printStackTrace();
+        }
+
+
+
         return new Object();
     }
 }
