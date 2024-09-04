68c2aa3e0a13ddda6c3dd4423dcbb6fe3c5f89f6
lijingze <lijingze@qq.com>
2024-09-04 16:34:22 +0800

    行为执行传入actionApi

--- ontology/ontology-rule-executor/src/main/java/com/aircas/ptr/foundry/rule/executor/controller/RuleController.java
+++ ontology/ontology-rule-executor/src/main/java/com/aircas/ptr/foundry/rule/executor/controller/RuleController.java
@@ -67,10 +67,15 @@ public class RuleController {
                         return null;
                     }
                     // 执行规则绑定的函数信息
-                    ActionHandleParam param = new ActionHandleParam();
-                    param.setApi(meta.getApiName());
-                    param.setPrimaryKey(value.toString());
-                    ontologyServer.actionExecute(param);
+                    for (OntologyAction action : ontologyActions){
+                        if (action.getId().longValue() == rule.getActionId().longValue()){
+                            ActionHandleParam param = new ActionHandleParam();
+                            param.setApi(action.getApi());
+                            param.setPrimaryKey(value.toString());
+                            ontologyServer.actionExecute(param);
+                        }
+                    }
+
                 }else{
                     log.info("规则校验未通过，规则内容："+rule.getRules());
                 }
