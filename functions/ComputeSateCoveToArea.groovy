1b1b5d28a925bb9b0f2b4422189ee0e5a8efb952
xiehuiping <xiehuiping@qq.com>
2025-12-03 20:10:12 +0800

    增加卫星对点和面目标的覆盖函数

---
+++ functions/ComputeSateCoveToArea.groovy
@@ -0,0 +1,107 @@
+import com.aircas.ptr.foundry.common.util.HttpUtil
+import com.aircas.ptr.foundry.ontology.common.vo.BaseSatelliteResp
+import com.aircas.ptr.foundry.ontology.model.vo.FunctionResultVO
+import com.fasterxml.jackson.core.type.TypeReference
+import groovy.transform.TupleConstructor
+import groovy.util.logging.Slf4j
+import org.apache.commons.collections.CollectionUtils
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+@TupleConstructor
+class J2000Position {
+
+    String time
+    Double vx
+    Double vy
+    Double vz
+    Double x
+    Double y
+    Double z
+}
+
+@TupleConstructor
+class Sensor {
+
+    String enum_sensor_type = "0"
+    SensorPointing sensor_pointing = new SensorPointing()
+}
+
+@TupleConstructor
+class SensorPointing {
+    Double azimuth = 0
+    Double elevation = 90
+}
+
+@TupleConstructor
+class Target {
+    Double lat
+    Double lon
+}
+
+@TupleConstructor
+class ComputeSateCoveToAreaReq {
+
+    Double dMaxRange = 0;
+
+    List<J2000Position> satOrbitDataList;
+
+    Sensor sensorDef = new Sensor()
+
+    List<Target> areaTarget
+}
+
+
+@TupleConstructor
+class TleParam {
+    String tle1
+    String tle2
+    String startTime
+    String endTime
+    Double interval
+    String type
+}
+
+
+@TupleConstructor
+class ComputeSateCoveToAreaVO {
+    String beginTime
+    String endTime
+    String topTime
+}
+
+@Slf4j
+class ComputeSateCoveToArea {
+
+
+    FunctionResultVO<List<ComputeSateCoveToAreaVO>> handle(String tle1, String tle2, List<Double> targetLon, List<Double> targetLat) {
+
+        var url = "http://192.168.11.107:8088/api/v1/satellite/getXYZPointByTLE"
+        LocalDateTime start = LocalDateTime.now();
+        LocalDateTime end = start.plusDays(1)
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
+        var param = new TleParam(tle1, tle2, start.format(formatter), end.format(formatter), 30, "SGP4")
+        BaseSatelliteResp<List<J2000Position>> resp = HttpUtil.postJson(url, new HashMap<String, String>(), param, new TypeReference<BaseSatelliteResp<List<J2000Position>>>() {
+        })
+        List<J2000Position> positions = resp.data
+        url = "http://192.168.11.107:8088/api/v1/satellite/computeSateCoveToArea"
+        var req = new ComputeSateCoveToAreaReq()
+        req.setSatOrbitDataList(positions)
+        List<Target> targets = new ArrayList<>()
+        for (int i = 0; i < targetLon.size(); i++) {
+            targets.add(new Target(targetLat[i], targetLon[i]))
+        }
+        req.setAreaTarget(targets)
+        var result = HttpUtil.postJson(url, new HashMap<String, String>(), req, new TypeReference<BaseSatelliteResp<List<ComputeSateCoveToAreaVO>>>() {
+        })
+        var vos = result.getData()
+        if (CollectionUtils.isEmpty(vos)) {
+            return FunctionResultVO.builder().data(vos).build();
+        } else {
+            var vo = vos.get(0);
+            return FunctionResultVO.builder().startTime(vo.beginTime).endTime(vo.endTime).data(vos).build()
+        }
+    }
+
+}
