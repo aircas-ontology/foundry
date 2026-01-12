811b2396f186c4d802122920545cb9905acca8a9
xiehuiping <xiehuiping@qq.com>
2026-01-12 11:00:36 +0800

    添加orekit data配置

---
+++ ontology/ontology-server/src/main/java/com/aircas/ptr/foundry/ontology/config/OrekitDataConfig.java
@@ -0,0 +1,31 @@
+package com.aircas.ptr.foundry.ontology.config;
+
+import org.orekit.data.DataContext;
+import org.orekit.data.DataProvidersManager;
+import org.orekit.data.DirectoryCrawler;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+
+@Configuration
+public class OrekitDataConfig {
+
+    @Value("${orekit.data.path}")
+    private String path;
+
+    @Bean
+    public DataProvidersManager dataProvidersManager() {
+        DataProvidersManager manager = DataContext.getDefault().getDataProvidersManager();
+        File orekitData = new File(path);
+
+        // 确保路径存在
+        if (orekitData.exists()) {
+            manager.addProvider(new DirectoryCrawler(orekitData));
+        } else {
+            throw new RuntimeException("orekit-data resource directory not found: " + path);
+        }
+        return manager;
+    }
+}
