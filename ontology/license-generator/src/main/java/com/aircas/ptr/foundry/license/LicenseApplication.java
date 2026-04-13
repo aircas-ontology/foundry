07435217884e97e060a357f4b4191d67edbd6cc6
liuyue <liuyue@iecas.ac.cn>
2026-04-13 10:54:34 +0800

    LicenseApplication优化

--- ontology/license-generator/src/main/java/com/aircas/ptr/foundry/license/LicenseApplication.java
+++ ontology/license-generator/src/main/java/com/aircas/ptr/foundry/license/LicenseApplication.java
@@ -1,11 +1,14 @@
 package com.aircas.ptr.foundry.license;
 
+import com.aircas.ptr.foundry.license.param.LicenseCheckModel;
 import com.aircas.ptr.foundry.license.param.LicenseCreatorParam;
 import com.aircas.ptr.foundry.license.service.LicenseService;
 import com.aircas.ptr.foundry.license.util.NetworkUtil;
 import lombok.var;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 public class LicenseApplication {
 
@@ -23,6 +26,27 @@ public class LicenseApplication {
 
     public static void main(String[] args) {
 
+        LicenseCheckModel licenseCheckModel;
+        /**
+         * 手动输入参数：ip 和 mac地址
+         */
+        if (args.length == 2 && args[0].startsWith("ip=") && args[1].startsWith("mac=")) {
+
+            var ipAddresses = args[0].substring(3);
+            var macAddresses = args[1].substring(4);
+
+            var ipAddress = Arrays.asList(ipAddresses.split(","));
+            var macAddress = Arrays.asList(macAddresses.split(","));
+
+            licenseCheckModel = LicenseCheckModel.builder()
+                    .ipAddress(ipAddress)
+                    .macAddress(macAddress)
+                    .build();
+
+        } else {
+            licenseCheckModel = NetworkUtil.getNetworkInfo();
+        }
+
         var licenseService = new LicenseService();
         licenseService.generateLicense(LicenseCreatorParam.builder()
                 .subject(SUBJECT)
@@ -33,7 +57,7 @@ public class LicenseApplication {
                 .keysStorePath(PRIVATE_KEYS_STORE_PATH)
                 .issuedTime(new Date())
                 .expiryTime(new Date(System.currentTimeMillis() + EXPIRE_TIME))
-                .licenseCheckModel(NetworkUtil.getNetworkInfo())
+                .licenseCheckModel(licenseCheckModel)
                 .build());
     }
 
